933bc20fd2d84d610df08f5fa73b1285b548cbf7
Hasla <richkibz@gmail.com>
2023-07-25 02:29:04 +0300

    loaders

---
+++ 0x00-Webpack/task_2/webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'production',
+
+  entry: path.resolve(__dirname, 'js/dashboard_main.js'),
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [
+        'style-loader',
+        'css-loader',
+        ]
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          'file-loader',
+          {
+            loader: 'image-webpack-loader',
+            options: {
+                bypassOnDebug: true,
+                disable: true,
+            }
+          },
+        ],
+      },
+    ]
+  }
+};
