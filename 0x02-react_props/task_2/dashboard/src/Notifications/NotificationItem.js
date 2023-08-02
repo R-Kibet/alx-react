d36c8f2e14f409e9c750137a53b464562663eec8
Hasla <richkibz@gmail.com>
2023-08-02 23:33:09 +0300

    create new test and add splits to notification"

---
+++ 0x02-react_props/task_2/dashboard/src/Notifications/NotificationItem.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function NotificationItem({ type, html, value }) {
+  let li;
+
+  value
+  ? (li = <li data-notification-type={type}>{value}</li>)
+  : (li = (
+      <li data-notification-type={type} dangerouslySetInnerHTML={html}></li>
+  ));
+  return li;
+}
