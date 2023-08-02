3c198d5032ddd8667571cc55c5fbb8580fd80982
Hasla <richkibz@gmail.com>
2023-08-02 23:33:47 +0300

    apend notification in task_2

---
+++ 0x02-react_props/task_2/dashboard/src/App/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import  Header from '../Header/Header';
+import Login from '../Login/Login';
+import Notifications from '../Notifications/Notifications';
+import Footer from '../Footer/Footer';
+
+function App() {
+  return (
+    <>
+    <Notifications />
+    <Header />
+    <Login />
+    <Footer />
+    </>
+  );
+}
+
+export default App;
