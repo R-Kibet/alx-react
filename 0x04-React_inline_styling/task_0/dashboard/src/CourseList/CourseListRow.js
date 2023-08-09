eb927875fc32acaf0a674825cc9c59add3ce2b68
Hasla <richkibz@gmail.com>
2023-08-09 19:29:00 +0300

    adding a const

--- 0x04-React_inline_styling/task_0/dashboard/src/CourseList/CourseListRow.js
+++ 0x04-React_inline_styling/task_0/dashboard/src/CourseList/CourseListRow.js
@@ -1,36 +1,38 @@
 import React from "react";
 import PropTypes from 'prop-types'
 
+const bkgH = {backgroundColor: '#f5f5f5ab'};
+const bkgR = {backgroundColor: '#deb5b545'}
 
-    export default function CourseListRow({isHeader, textFirstCell, textSecondCell}) {
-      return (
-        <tr style={{backgroundColor: '#f5f5f5ab'}}>
-            {isHeader ? (
-                textSecondCell === null ? (
-                    <th colSpan={2}>{textFirstCell}</th>
-                ) : (
-                    <>
-                    <th style={{backgroundColor: '#deb5b545'}}>{textFirstCell}</th>
-                    <th style={{backgroundColor: '#deb5b545'}}>{textSecondCell}</th>
-                    </>
-                )
+export default function CourseListRow({isHeader, textFirstCell, textSecondCell}) {
+    return (
+    <tr style={bkgH}>
+        {isHeader ? (
+            textSecondCell === null ? (
+                <th colSpan={2}>{textFirstCell}</th>
             ) : (
                 <>
-                <td>{textFirstCell}</td>
-                <td>{textSecondCell}</td>
+                <th style={bkgR}>{textFirstCell}</th>
+                <th style={bkgR}>{textSecondCell}</th>
                 </>
-            )}
-        </tr>
-      )
-    }
+            )
+        ) : (
+            <>
+            <td>{textFirstCell}</td>
+            <td>{textSecondCell}</td>
+            </>
+        )}
+    </tr>
+    )
+}
 
-    CourseListRow.PropTypes = {
-        isHeader: PropTypes.bool,
-        textFirstCell: PropTypes.string,
-        textSecondCell: PropTypes.oneOfType([PropTypes.string, PropTypes.number])
-    }
+CourseListRow.PropTypes = {
+    isHeader: PropTypes.bool,
+    textFirstCell: PropTypes.string,
+    textSecondCell: PropTypes.oneOfType([PropTypes.string, PropTypes.number])
+}
 
-    CourseListRow.defaultProps = {
-        isHeader: false,
-        textSecondCell: null
-    }
+CourseListRow.defaultProps = {
+    isHeader: false,
+    textSecondCell: null
+}
