16960eb59c23dc0a9d7f201d2377f27d9886bd77
Hasla <richkibz@gmail.com>
2023-08-14 22:47:11 +0300

    check login

--- 0x05-react_state/task_1/dashboard/src/Login/Login.js
+++ 0x05-react_state/task_1/dashboard/src/Login/Login.js
@@ -1,19 +1,74 @@
 import React from 'react';
 import { StyleSheet, css } from 'aphrodite';
 
-function App() {
-  return (
-    <>
-      <main role='main' className={css(styles.Abody)}>
-        <p>Login to access the full dashboard</p>
-        <label htmlFor='email'>Email</label>
-        <input type='email' id='email' className={css(styles.inpt)}/>
-        <label className={css(styles.lbl)} htmlFor='password'>Password</label>
-        <input type='password' id='password' className={css(styles.inpt)} />
-        <button className={css(styles.btn)} type='button'>OK</button>
-      </main>
+class Login extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      isLoggedIn: props.isLoggedIn,
+      email: '',
+      passwword:'',
+      enableSubmit: false,
+    };
+
+    this.handleLoginSubmit = this.handleLoginSubmit.bind(this);
+    this.handleChangeEmail = this.handleChangeEmail.bind(this);
+    this.handleChangePassword = this.handleChangePassword.bind(this);
+
+  }
+
+  handleLoginSubmit(event) {
+   event.preventDefault();
+   this.setState({ isLoggedIn: true });
+  }
+
+  handleChangeEmail(event) {
+   const { value } = event.target;
+   const { password } = this.state;
+
+   if (value !== '' && password !== '') this.setState({ enableSubmit: true });
+   else this,this.setState({ enableSubmit: false });
+
+   this.setState({ email: event.target.value });
+  }
+
+  handleChangePassword(event) {
+   const { value } = event.target;
+   const { email } = this.state;
+
+   if (email !== '' && value !== '') this.state({ enableSubmit: true });
+   else this.setState({ enableSubmit: false });
+
+   this.setState({ password: event.target.value });
+
+  }
+  render(){
+    return (
+      <>
+      <div role='main' className={css(styles.Abody)}>
+          <p>Login to access the full dashboard</p>
+          <form action='' onSubmit={this.handleLoginSubmit}>
+           <label htmlFor='email'>Email</label>
+           <input 
+             type='email' 
+             id='email' 
+             className={css(styles.inpt)}
+             value={this.state.email}
+             onChange={this.handleChangeEmail}
+             />
+           <label className={css(styles.lbl)} htmlFor='password'>Password</label>
+           <input 
+             type='password' 
+             id='password' 
+             className={css(styles.inpt)} 
+             value={this.state.password}
+             onChange={this.handleChangePassword}
+             />
+           <input type='submit' disabled={!this.state.enableSubmit}></input>
+          </form>
+      </div>
     </>
-  );
+  )};
 }
 
 /* responsive to screen size */
@@ -59,4 +114,4 @@ const styles =  StyleSheet.create({
 
 })
 
-export default App;
+export default Login;
