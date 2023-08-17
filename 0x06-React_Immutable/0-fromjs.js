ca0c2258bc87197cbd05a85cd915e54cee243336
Kibenje <58045991+R-Kibet@users.noreply.github.com>
2023-08-17 11:31:18 +0300

    Delete 0x06-React_Immutable directory

--- 0x06-React_Immutable/0-fromjs.js
+++
@@ -1,7 +0,0 @@
-import Immutable from "./node_modules/immutable/dist/immutable";
-
-function getImmutableObject(object) {
-  return Immutable.fromJS(object)
-};
-
-export default getImmutableObject;
