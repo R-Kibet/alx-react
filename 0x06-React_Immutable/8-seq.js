8ba7859c439a5cb50df2648e07ad167ccf3d2413
Hasla <richkibz@gmail.com>
2023-08-17 16:40:30 +0300

    list element

--- 0x06-React_Immutable/8-seq.js
+++ 0x06-React_Immutable/8-seq.js
@@ -6,7 +6,7 @@ export default function printBestStudents(obj) {
     .map((std) => ({
       ...std,
       firstName: std.firstName.charAt(0).toUpperCase() + std.firstName.slice(1),
-      lastName: std.lastName.charAt(0).toUpperCase() + std.lastName.slice(1)
+      lastName: std.lastName.charAt(0).toUpperCase() + std.lastName.slice(1),
     }))
     .toObject();
 
