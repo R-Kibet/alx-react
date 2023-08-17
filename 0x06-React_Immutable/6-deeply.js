85ccf470f86d471be2b5f7d1fd2fe5d86aa8ded9
Hasla <richkibz@gmail.com>
2023-08-17 15:42:38 +0300

    merge deep and map

---
+++ 0x06-React_Immutable/6-deeply.js
@@ -0,0 +1,8 @@
+import { Map } from './node_modules/immutable/dist/immutable';
+
+export default function mergeDeeplyElements(page1, page2) {
+  const m1 = Map(page1);
+  const m2 = Map(page2);
+
+  return m1.mergeDeep(m2);
+}
