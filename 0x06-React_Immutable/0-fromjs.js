c92e292228b53dedb8284ee8bf0924be58c55fc2
Hasla <richkibz@gmail.com>
2023-08-17 11:46:55 +0300

    using fromjs method in immutale

--- 0x06-React_Immutable/0-fromjs.js
+++ 0x06-React_Immutable/0-fromjs.js
@@ -2,6 +2,6 @@ import Immutable from "./node_modules/immutable/dist/immutable";
 
 function getImmutableObject(object) {
   return Immutable.fromJS(object)
-};
+}
 
 export default getImmutableObject;
