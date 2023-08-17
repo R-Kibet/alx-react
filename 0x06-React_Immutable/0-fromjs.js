b950cef93128a0c8b1f0886f5189bf49819bb670
Hasla <richkibz@gmail.com>
2023-08-17 12:11:30 +0300

    using fromjs

--- 0x06-React_Immutable/0-fromjs.js
+++ 0x06-React_Immutable/0-fromjs.js
@@ -1,7 +1,7 @@
-import Immutable from "./node_modules/immutable/dist/immutable";
+import Immutable from './node_modules/immutable/dist/immutable';
 
 function getImmutableObject(object) {
-  return Immutable.fromJS(object)
+  return Immutable.fromJS(object);
 }
 
 export default getImmutableObject;
