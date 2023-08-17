b7b3ae9e59051f270d373aab1d7c6e4c7c4f86e6
Hasla <richkibz@gmail.com>
2023-08-17 16:36:27 +0300

    using seq method

---
+++ 0x06-React_Immutable/8-seq.js
@@ -0,0 +1,14 @@
+import { Seq } from './node_modules/immutable/dist/immutable';
+
+export default function printBestStudents(obj) {
+  const student = Seq(obj)
+    .filter((std) => std.score >= 70)
+    .map((std) => ({
+      ...std,
+      firstName: std.firstName.charAt(0).toUpperCase() + std.firstName.slice(1),
+      lastName: std.lastName.charAt(0).toUpperCase() + std.lastName.slice(1)
+    }))
+    .toObject();
+
+  console.log(student);
+}
