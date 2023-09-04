d842c2fbdc4439695dca28162fdb26f13070ee1b
Hasla <richkibz@gmail.com>
2023-09-04 23:18:26 +0300

    modifyning login and logout user

---
+++ 0x09-react_redux_connectors_and_providers/task_2/dashboard/src/reducers/uiReducer.js
@@ -0,0 +1,42 @@
+import {
+  LOGOUT,
+  LOGIN_SUCCESS,
+  LOGIN_FAILURE,
+  HIDE_NOTIFICATION_DRAWER,
+  DISPLAY_NOTIFICATION_DRAWER,
+  LOGIN,
+} from "../actions/uiActionTypes";
+
+import { Map } from "immutable";
+
+export const initialState = {
+  user: {},
+  isUserLoggedIn: false,
+  isNotificationDrawerVisible: false,
+};
+
+export default function uiReducer(state = Map(initialState), action) {
+  switch (action.type) {
+    case LOGIN:
+      return state.set("user", action.user);
+
+    case LOGIN_SUCCESS:
+      return state.set("isUserLoggedIn", true);
+
+    case LOGIN_FAILURE:
+      return state.set("isUserLoggedIn", false);
+
+    case DISPLAY_NOTIFICATION_DRAWER:
+      return state.set("isNotificationDrawerVisible", true);
+      
+    case HIDE_NOTIFICATION_DRAWER:
+      return state.set("isNotificationDrawerVisible", false);
+
+    case LOGOUT:
+      return state.set("isUserLoggedIn", false).set("user", null);
+
+    default:
+      break;
+  }
+  return state;
+}
