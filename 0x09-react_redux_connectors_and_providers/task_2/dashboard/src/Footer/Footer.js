f036ab6abd729124159aae29e8294a5e83d53c83
Hasla <richkibz@gmail.com>
2023-09-04 22:58:23 +0300

    implementing user state to footer

---
+++ 0x09-react_redux_connectors_and_providers/task_2/dashboard/src/Footer/Footer.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { getFullYear, getFooterCopy } from "../utils/utils";
+import AppContext from "../App/AppContext";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+
+function Footer(user) {
+  return (
+    <div className="footer">
+      <p>
+        Copyright {getFullYear()} - {getFooterCopy(true)}
+      </p>
+      {user && <a href="#">Contact us</a>}
+    </div>
+  );
+}
+
+Footer.defaultProps = {
+  user: null,
+};
+
+Footer.propTypes = {
+  user: PropTypes.object,
+};
+
+const mapStateToProps = (state) => {
+  return {
+    user: state.get("user"),
+  };
+};
+
+export default connect(mapStateToProps, null)(Footer);
