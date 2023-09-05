631e65c9de3f9feb2882ba6d6e5489a579a806b3
Hasla <richkibz@gmail.com>
2023-09-05 11:28:47 +0300

    implementing react dev tools

--- 0x09-react_redux_connectors_and_providers/task_3/dashboard/src/index.js
+++ 0x09-react_redux_connectors_and_providers/task_3/dashboard/src/index.js
@@ -4,10 +4,15 @@ import App from "./App/App";
 import { createStore, applyMiddleware } from "redux";
 import uiReducer, { initialState } from "./reducers/uiReducer";
 import { Provider } from "react-redux";
-import thunk from 'redux-thunk'
+import thunk from "redux-thunk";
 import { Map } from "immutable";
+import { composeWithDevTools } from "redux-devtools-extension";
 
-const store = createStore(uiReducer, Map(initialState),  applyMiddleware(thunk));
+const store = createStore(
+  uiReducer,
+  Map(initialState),
+  composeWithDevTools(applyMiddleware(thunk))
+);
 
 ReactDOM.render(
   <React.StrictMode>
