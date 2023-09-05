55ad3cd56d33532c48ea228cb361239f8dd8f783
Hasla <richkibz@gmail.com>
2023-09-05 21:29:56 +0300

    implementing selector

--- 0x09-react_redux_connectors_and_providers/task_6/dashboard/src/Notifications/Notifications.js
+++ 0x09-react_redux_connectors_and_providers/task_6/dashboard/src/Notifications/Notifications.js
@@ -219,6 +219,7 @@ const styles = StyleSheet.create({
 });
 
 const mapStateToProps = (state) => {
+  //implementing selector
   const unread = getUnreadNotifications(state)
   return {
     listNotifications: unread,
